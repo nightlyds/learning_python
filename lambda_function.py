fecda8d01fcfc8caebb17c0f4e386194ff81b081
Danyl Safonov <safonov.danil.cw@gmail.com>
2021-04-28 17:33:23 +0300

    Lambda

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+# Lambda functions
+implementation_lambda = lambda x: x * 2
+
+print(implementation_lambda(4)) # Output: 8
+
+# or
+
+print((lambda x: x * 2)(4)) # Output: 8
+
+def implementation_function(num):
+    return lambda x: x * num
+
+implementation_function_with_lambda = implementation_function(90)
+
+print(implementation_function_with_lambda(11)) # Output: 990
